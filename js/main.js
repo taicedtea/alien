3ad7684ecd8bb03bed9439a256e7b8f0dfcbfde5
taicedtea <taiwdisla@gmail.com>
2026-05-29 02:16:55 -0400

    3D modeling showing up, able to rotate and zoom

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 //Import three.js library
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.184.0/build/three.module.js";
+import * as THREE from "three";
 //Import orbit controls
-import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.184.0/examples/jsm/controls/OrbitControls.js"
+import { OrbitControls } from "three/addons/controls/OrbitControls.js"
 //Allow importing of .gltf
-import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.184.0/examples/jsm/loaders/GLTFLoader.js"
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js"
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -11,13 +11,13 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 let object;
 let controls;
 
-let objToRender = 'Alien'
+let objToRender = 'alien'
 
 const loader = new GLTFLoader();
 
 // Load file
 loader.load(
-    'models/${objToRender}/scene.gltf',
+    `assets/${objToRender}.gltf`,
     function (gltf) {
         // If file is loaded, add to scene
         object = gltf.scene;
@@ -32,3 +32,42 @@ loader.load(
         console.error(error);
     }
 )
+
+//Create Renderer
+const renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+//Add renderer to DOM
+document.getElementById("container3D").appendChild(renderer.domElement);
+
+//Set camera position
+camera.position.z = objToRender == 'alien' ? 2 : 500;
+
+//Add lights to scene
+const topLight = new THREE.DirectionalLight(0xffffff, 1);
+topLight.position.set(10, 20, 1000)
+topLight.castShadow = true;
+scene.add(topLight);
+
+const ambientLight = new THREE.AmbientLight(0x333333, objToRender == 'alien' ? 5 : 1);
+scene.add(ambientLight);
+
+if (objToRender == 'alien') {
+    controls = new OrbitControls(camera, renderer.domElement);
+}
+
+//Render scene
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+//Allows to resize window with camera
+window.addEventListener("resize", function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+animate();
